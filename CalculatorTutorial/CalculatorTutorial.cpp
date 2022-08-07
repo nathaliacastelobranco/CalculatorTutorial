553f59548c8bb5779d683edb525acf272455a3ca
nathaliaacbranco <nathaliaacbranco@gmail.com>
2022-08-06 21:20:54 -0300

    Adicionar arquivos de projeto.

---
+++ CalculatorTutorial/CalculatorTutorial.cpp
@@ -0,0 +1,48 @@
+// CalculatorTutorial.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std; //faz com que todas as entradas padrão do c++ não solicitem o std::
+
+int main()
+{
+    double x = 0.0; //double aceita decimais, iniciando variaveis com formato double
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+
+    Calculator c; //declara um objeto chamado C como uma instância da classe Calculator
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y ==0)
+        {
+            cout << "Division by 0 exception" << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Result is: " << result << endl;
+    }
+
+    return 0;
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
